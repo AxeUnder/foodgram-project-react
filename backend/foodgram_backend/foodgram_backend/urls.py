71d676fce5924ca3d50cfc3da5ea7c1f9ddf2cbd
AxeUnder <yachmenyov.alex@yandex.ru>
2023-08-01 16:27:51 +0700

    Upgraded settings for linux, apps and models

--- backend/foodgram_backend/foodgram_backend/urls.py
+++ backend/foodgram_backend/foodgram_backend/urls.py
@@ -1,7 +1,7 @@
-"""foodgram_dackend URL Configuration
+"""foodgram_backend URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,15 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from django.views.generic import TemplateView
-
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path(
-        'redoc/',
-        TemplateView.as_view(template_name='redoc.html'),
-        name='redoc'
-    ),
 ]
--- backend/foodgram_backend/foodgram_backend/wsgi.py
+++ backend/foodgram_backend/foodgram_backend/wsgi.py
@@ -1,16 +1,16 @@
 """
-WSGI config for foodgram_dackend project.
+WSGI config for foodgram_backend project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'foodgram_dackend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'foodgram_backend.settings')
 
 application = get_wsgi_application()
