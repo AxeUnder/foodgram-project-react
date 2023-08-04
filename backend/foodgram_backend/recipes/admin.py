dc934e8cd0a8d9f838fc47c0c4e82ac8d2c3ccb9
AxeUnder <yachmenyov.alex@yandex.ru>
2023-08-04 17:19:29 +0700

    Upgrade the app Recipe admin, model

--- backend/foodgram_backend/recipes/admin.py
+++ backend/foodgram_backend/recipes/admin.py
@@ -10,14 +10,56 @@ class RecipeIngredientInline(admin.TabularInline):
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
-    pass
+    list_display = (
+        'pk',
+        'name',
+        'color',
+        'slug'
+    )
+    list_display_links = ('name',)
+    search_fields = ('name',)
+    empty_value_display = '-пусто-'
 
 
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
     inlines = (RecipeIngredientInline,)
+    list_display = (
+        'pk',
+        'name',
+        'tags',
+        'author',
+        'text',
+        'ingredients',
+        'image'
+    )
+    list_display_links = ('name',)
+    search_fields = (
+        'name',
+        'author',
+        'text',
+        'ingredients'
+    )
+    list_editable = (
+        'name',
+        'author',
+    )
+    list_filter = ('tags',)
+    empty_value_display = '-пусто-'
 
 
 @admin.register(Ingredient)
 class IngredientAdmin(admin.ModelAdmin):
-    pass
+    list_display = (
+        'pk',
+        'name',
+        'measurement_unit'
+    )
+    list_display_links = ('name',)
+    list_filter = ('measurement_unit',)
+    list_editable = (
+        'name',
+        'measurement_unit'
+    )
+    search_fields = ('name',)
+    empty_value_display = '-пусто-'
--- backend/foodgram_backend/recipes/models.py
+++ backend/foodgram_backend/recipes/models.py
@@ -1,44 +1,94 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.utils.translation import gettext_lazy as _
 
 
 User = get_user_model()
 
 
 class Tag(models.Model):
-    name = models.CharField(max_length=200)
-    color = models.CharField(max_length=7)
-    slug = models.SlugField(max_length=200, unique=True)
+    name = models.CharField(verbose_name=_('Имя тега'), max_length=200)
+    color = models.CharField(verbose_name=_('HEX-цвет тега'), max_length=7)
+    slug = models.SlugField(
+        verbose_name=_('Слуг'),
+        max_length=200,
+        unique=True
+    )
+
+    class Meta:
+        verbose_name = _('Тег')
+        verbose_name_plural = _('Теги')
+        ordering = ('id',)
 
 
 class Recipe(models.Model):
-    tags = models.ManyToManyField(Tag)
-    name = models.CharField(max_length=200)
-    cooking_time = models.PositiveIntegerField()
-    text = models.CharField(max_length=250)
+    tags = models.ManyToManyField(Tag, verbose_name=_('Тег'))
+    name = models.CharField(
+        verbose_name=_('Название рецепта'),
+        max_length=200
+    )
+    cooking_time = models.PositiveIntegerField(
+        verbose_name=_('Время готовки')
+    )
+    text = models.CharField(verbose_name=_('Текст'), max_length=250)
     image = models.ImageField(
+        verbose_name=_('Картинка'),
         upload_to='recipes',
         blank=True,
         null=True,
     )
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(
+        User,
+        verbose_name=_('Автор'),
+        on_delete=models.CASCADE
+    )
     ingredients = models.ManyToManyField(
         'Ingredient',
+        verbose_name=_('Ингредиенты'),
         through='RecipeIngredient',
         through_fields=('recipe', 'ingredient')
     )
 
+    class Meta:
+        verbose_name = _('Рецепт')
+        verbose_name_plural = _('Рецепты')
+        ordering = ('id',)
+
+    def __str__(self):
+        return self.name
+
 
 class Ingredient(models.Model):
-    name = models.CharField(max_length=200)
-    measurement_unit = models.CharField(max_length=200)
+    name = models.CharField(
+        verbose_name=_('Название ингредиента'),
+        max_length=200
+    )
+    measurement_unit = models.CharField(
+        verbose_name=_('Единица измерения'),
+        max_length=200
+    )
+
+    class Meta:
+        verbose_name = _('Ингредиент')
+        verbose_name_plural = _('Ингредиенты')
+        ordering = ('id',)
 
 
 class RecipeIngredient(models.Model):
     recipe = models.ForeignKey(
         Recipe,
+        verbose_name=_('Рецепт'),
         related_name='recipe_ingredients',
         on_delete=models.CASCADE
     )
-    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
-    amount = models.PositiveIntegerField()
+    ingredient = models.ForeignKey(
+        Ingredient,
+        verbose_name=_('Ингредиент'),
+        on_delete=models.CASCADE
+    )
+    amount = models.PositiveIntegerField(verbose_name=_('Количество'),)
+
+    class Meta:
+        verbose_name = _('Рецепт_Ингредиент')
+        verbose_name_plural = _('Рецепты_Ингредиенты')
+        ordering = ('id',)
