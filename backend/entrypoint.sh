f780f048778bd7722c1ef7b4bf33bd40e1b7395b
AxeUnder <yachmenyov.alex@yandex.ru>
2023-09-05 23:03:26 +0700

    Deploy 1.05

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,4 +1,6 @@
 python manage.py collectstatic --noinput
+cp -r /app/static/. /backend_static/
+cp -r /app/media/. /backend_media/
 python manage.py migrate
 python manage.py import_csv
 gunicorn foodgram.wsgi:application --bind 0:8080
